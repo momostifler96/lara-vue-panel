93acb1d43c69f624391f9cd546c37891f640a8df
Mohamed Doumbia <91441124+momostifler96@users.noreply.github.com>
2025-02-09 00:34:04 +0000

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ import preset from './tailwind.config.preset'
 export default {
     presets: [preset],
     content: [
-
         './vendor/momoledev/lvp/resources/js/**/*.{js,ts,vue}',
         './vendor/momoledev/lvp/resources/views/**/*.blade.php',
         './vendor/momoledev/lvp/src/**/*.php',
